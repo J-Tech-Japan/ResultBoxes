d5d26c7d11d23ddf98918e79fa1de8ca67ab94a7
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-05-16 12:48:10 -0700

    Add Tap Extensions

--- src/ResultBoxes/ResultBoxes/TapExtensions.cs
+++ src/ResultBoxes/ResultBoxes/TapExtensions.cs
@@ -19,12 +19,31 @@ public static class TapExtensions
         where TValue1 : notnull
         where TValue2 : notnull
         => Tap(result, values => values.CallAction(action));
+    
     public static ResultBox<ThreeValues<TValue1, TValue2, TValue3>> Tap<TValue1, TValue2, TValue3>(
         this ResultBox<ThreeValues<TValue1, TValue2, TValue3>> result,
         Action<TValue1, TValue2, TValue3> action)
         where TValue1 : notnull
         where TValue2 : notnull
         where TValue3 : notnull
+        => Tap(result, values => values.CallAction(action));
+
+    public static ResultBox<FourValues<TValue1, TValue2, TValue3, TValue4>> Tap<TValue1, TValue2, TValue3, TValue4>(
+        this ResultBox<FourValues<TValue1, TValue2, TValue3, TValue4>> result,
+        Action<TValue1, TValue2, TValue3, TValue4> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        => Tap(result, values => values.CallAction(action));
+    public static ResultBox<FiveValues<TValue1, TValue2, TValue3, TValue4, TValue5>> Tap<TValue1, TValue2, TValue3, TValue4, TValue5>(
+        this ResultBox<FiveValues<TValue1, TValue2, TValue3, TValue4, TValue5>> result,
+        Action<TValue1, TValue2, TValue3, TValue4, TValue5> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        where TValue5 : notnull
         => Tap(result, values => values.CallAction(action));
 
     public static async Task<ResultBox<TValue>> Tap<TValue>(
@@ -45,6 +64,32 @@ public static class TapExtensions
         where TValue2 : notnull
         => await Tap(result, async values => await values.CallAction(action));
 
+    public static async Task<ResultBox<ThreeValues<TValue1, TValue2,TValue3>>> Tap<TValue1, TValue2, TValue3>(
+        this ResultBox<ThreeValues<TValue1, TValue2,TValue3>> result,
+        Func<TValue1, TValue2,TValue3, Task> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        => await Tap(result, async values => await values.CallAction(action));
+
+    public static async Task<ResultBox<FourValues<TValue1, TValue2,TValue3, TValue4>>> Tap<TValue1, TValue2, TValue3, TValue4>(
+        this ResultBox<FourValues<TValue1, TValue2,TValue3, TValue4>> result,
+        Func<TValue1, TValue2,TValue3, TValue4, Task> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        => await Tap(result, async values => await values.CallAction(action));
+    public static async Task<ResultBox<FiveValues<TValue1, TValue2,TValue3, TValue4, TValue5>>> Tap<TValue1, TValue2, TValue3, TValue4, TValue5>(
+        this ResultBox<FiveValues<TValue1, TValue2,TValue3, TValue4, TValue5>> result,
+        Func<TValue1, TValue2,TValue3, TValue4, TValue5, Task> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        where TValue5 : notnull
+        => await Tap(result, async values => await values.CallAction(action));
+
     public static async Task<ResultBox<TValue>> Tap<TValue>(
         this Task<ResultBox<TValue>> result,
         Action<TValue> action)
@@ -62,6 +107,31 @@ public static class TapExtensions
         Action<TValue1, TValue2> action)
         where TValue1 : notnull
         where TValue2 : notnull
+        => (await result).Tap(values => values.CallAction(action));
+    public static async Task<ResultBox<ThreeValues<TValue1, TValue2, TValue3>>> Tap<TValue1, TValue2, TValue3>(
+        this Task<ResultBox<ThreeValues<TValue1, TValue2, TValue3>>> result,
+        Action<TValue1, TValue2, TValue3> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+    where TValue3 : notnull
+        => (await result).Tap(values => values.CallAction(action));
+
+    public static async Task<ResultBox<FourValues<TValue1, TValue2, TValue3, TValue4>>> Tap<TValue1, TValue2, TValue3, TValue4>(
+        this Task<ResultBox<FourValues<TValue1, TValue2, TValue3, TValue4>>> result,
+        Action<TValue1, TValue2, TValue3, TValue4> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        => (await result).Tap(values => values.CallAction(action));
+    public static async Task<ResultBox<FiveValues<TValue1, TValue2, TValue3, TValue4, TValue5>>> Tap<TValue1, TValue2, TValue3, TValue4, TValue5>(
+        this Task<ResultBox<FiveValues<TValue1, TValue2, TValue3, TValue4, TValue5>>> result,
+        Action<TValue1, TValue2, TValue3, TValue4, TValue5> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        where TValue5 : notnull
         => (await result).Tap(values => values.CallAction(action));
 
     public static async Task<ResultBox<TValue>> Tap<TValue>(
@@ -81,5 +151,29 @@ public static class TapExtensions
         Func<TValue1, TValue2, Task> action)
         where TValue1 : notnull
         where TValue2 : notnull
+        => await (await result).Tap(async values => await values.CallAction(action));
+    public static async Task<ResultBox<ThreeValues<TValue1, TValue2, TValue3>>> Tap<TValue1, TValue2, TValue3>(
+        this Task<ResultBox<ThreeValues<TValue1, TValue2, TValue3>>> result,
+        Func<TValue1, TValue2, TValue3, Task> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        => await (await result).Tap(async values => await values.CallAction(action));
+    public static async Task<ResultBox<FourValues<TValue1, TValue2, TValue3, TValue4>>> Tap<TValue1, TValue2, TValue3, TValue4>(
+        this Task<ResultBox<FourValues<TValue1, TValue2, TValue3, TValue4>>> result,
+        Func<TValue1, TValue2, TValue3, TValue4, Task> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        => await (await result).Tap(async values => await values.CallAction(action));
+    public static async Task<ResultBox<FiveValues<TValue1, TValue2, TValue3, TValue4, TValue5>>> Tap<TValue1, TValue2, TValue3, TValue4, TValue5>(
+        this Task<ResultBox<FiveValues<TValue1, TValue2, TValue3, TValue4, TValue5>>> result,
+        Func<TValue1, TValue2, TValue3, TValue4, TValue5, Task> action)
+        where TValue1 : notnull
+        where TValue2 : notnull
+        where TValue3 : notnull
+        where TValue4 : notnull
+        where TValue5 : notnull
         => await (await result).Tap(async values => await values.CallAction(action));
 }
