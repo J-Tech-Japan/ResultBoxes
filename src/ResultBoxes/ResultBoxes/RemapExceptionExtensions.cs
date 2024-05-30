bc6a39e19b4c3598180f67ed3e35b135c4c6a9d1
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-05-30 00:03:24 -0700

    feat: Add RemapExceptionSpec test class and RemapExceptionExtensions
    This commit adds the RemapExceptionSpec test class and the RemapExceptionExtensions class to the ResultBoxes project. The RemapExceptionSpec class contains unit tests for the RemapException method, while the RemapExceptionExtensions class provides extension methods for remapping exceptions in ResultBox objects.

---
+++ src/ResultBoxes/ResultBoxes/RemapExceptionExtensions.cs
@@ -0,0 +1,49 @@
+namespace ResultBoxes;
+
+public static class RemapExceptionExtensions
+{
+    public static ResultBox<TValue> RemapException<TValue>(
+        this ResultBox<TValue> current,
+        Func<Exception, Exception> remapExceptionFunc)
+        where TValue : notnull
+        =>
+            current switch
+            {
+                { IsSuccess: false } => remapExceptionFunc(current.GetException()),
+                { IsSuccess: true } => current.GetValue()
+            };
+
+    public static async Task<ResultBox<TValue>> RemapException<TValue>(
+        this ResultBox<TValue> current,
+        Func<Exception, Task<Exception>> remapExceptionFuncAsync)
+        where TValue : notnull
+        =>
+            current switch
+            {
+                { IsSuccess: false } => await remapExceptionFuncAsync(current.GetException()),
+                { IsSuccess: true } => ResultBox.FromValue(current.GetValue()) 
+            };
+
+    public static async Task<ResultBox<TValue>> RemapException<TValue>(
+        this Task<ResultBox<TValue>> current,
+        Func<Exception, Exception> remapExceptionFunc)
+        where TValue : notnull
+        =>
+            await current switch
+            {
+                { IsSuccess: false } valueBox => remapExceptionFunc(valueBox.GetException()),
+                { IsSuccess: true } errorBox => ResultBox.FromValue(errorBox.GetValue()) 
+            };
+
+    public static async Task<ResultBox<TValue>> RemapException<TValue>(
+        this Task<ResultBox<TValue>> current,
+        Func<Exception, Task<Exception>> remapExceptionFuncAsync)
+        where TValue : notnull
+        =>
+            await current switch
+            {
+                { IsSuccess: false } valueBox => await remapExceptionFuncAsync(valueBox.GetException()),
+                { IsSuccess: true } errorBox => ResultBox.FromValue(errorBox.GetValue()) 
+            };
+
+}
