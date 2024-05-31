cb8711bd5fac04bdff29f5189939be7d5c54cfc0
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-05-30 22:58:52 -0700

    ScanEach and Reduce

---
+++ src/ResultBoxes/ResultBoxes.Test/ScanEachSpec.cs
@@ -0,0 +1,98 @@
+using Xunit.Abstractions;
+namespace ResultBoxes.Test;
+
+public class ScanEachSpec(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void ScanEachTest1()
+    {
+        var result = ResultBox.FromValue(Enumerable.Range(0, 10).ToList())
+            .ScanEach(i =>
+            {
+                testOutputHelper.WriteLine(i.ToString());
+                return ScanEachControlFlow.Continue;
+            });
+        Assert.True(result.IsSuccess);
+    }
+    [Fact]
+    public void ScanEachTest2()
+    {
+        var result = ResultBox.FromValue(Enumerable.Range(0, 10).ToList())
+            .ScanEach(i =>
+            {
+                testOutputHelper.WriteLine(i.ToString());
+                return i < 5 ? ScanEachControlFlow.Continue : ScanEachControlFlow.Break;
+            });
+        Assert.True(result.IsSuccess);
+    }
+    [Fact]
+    public void ScanEachTest3()
+    {
+        var result = ResultBox.FromValue(Enumerable.Range(0, 10).ToList())
+            .ScanEach(i => testOutputHelper.WriteLine(i.ToString()));
+        Assert.True(result.IsSuccess);
+    }
+    [Fact]
+    public async Task ScanEachTest1Async()
+    {
+        var result = await ResultBox.FromValue(Enumerable.Range(0, 10).ToList())
+            .ScanEach(async i =>
+            {
+                await Task.CompletedTask;
+                testOutputHelper.WriteLine(i.ToString());
+            });
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task ScanEachTest2Async()
+    {
+        var result = await ResultBox.FromValue(Enumerable.Range(0, 10).ToList())
+            .ScanEach(async i =>
+            {
+                await Task.CompletedTask;
+                testOutputHelper.WriteLine(i.ToString());
+                return i < 5 ? ScanEachControlFlow.Continue : ScanEachControlFlow.Break;
+            });
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task ScanEachTest3Async()
+    {
+        var result = await ResultBox.FromValue(Task.FromResult(Enumerable.Range(0, 10).ToList()))
+            .ScanEach(async i =>
+            {
+                await Task.CompletedTask;
+                testOutputHelper.WriteLine(i.ToString());
+            });
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task ScanEachTest4Async()
+    {
+        var result = await ResultBox.FromValue(Task.FromResult(Enumerable.Range(0, 10).ToList()))
+            .ScanEach(async i =>
+            {
+                await Task.CompletedTask;
+                testOutputHelper.WriteLine(i.ToString());
+                return i < 5 ? ScanEachControlFlow.Continue : ScanEachControlFlow.Break;
+            });
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task ScanEachTest5Async()
+    {
+        var result = await ResultBox.FromValue(Task.FromResult(Enumerable.Range(0, 10).ToList()))
+            .ScanEach(async i =>
+            {
+                await Task.CompletedTask;
+                testOutputHelper.WriteLine(i.ToString());
+            });
+        Assert.True(result.IsSuccess);
+    }
+
+}
+
---
+++ src/ResultBoxes/ResultBoxes/ReduceExtensions.cs
@@ -0,0 +1,51 @@
+namespace ResultBoxes;
+
+public static class ReduceExtensions
+{
+    public static ResultBox<TValueResult> ReduceEach<TValue, TValueResult>(
+        this ResultBox<List<TValue>> current,
+        TValueResult initialValue,
+        Func<TValue, TValueResult, ResultBox<ReduceResultValue<TValueResult>>> reduceFunc)
+        where TValue : notnull
+        where TValueResult : notnull
+    {
+        if (!current.IsSuccess)
+        {
+            return ResultBox.FromException<TValueResult>(current.GetException());
+        }
+        var result = ResultBox.FromValue(ReduceResultValue.Start(initialValue));
+        foreach (var value in current.GetValue())
+        {
+            result = reduceFunc(value, result.GetValue().Value);
+            if (!result.IsSuccess || result.GetValue().ControlFlow == ReduceControlFlow.Break)
+            {
+                break;
+            }
+        }
+        return result.IsSuccess ? ResultBox.FromValue(result.GetValue().Value) : result.GetException();
+    }
+
+    public static ResultBox<TValueResult> ReduceEach<TValue, TValueResult>(
+        this ResultBox<List<TValue>> current,
+        TValueResult initialValue,
+        Func<TValue, TValueResult, ResultBox<TValueResult>> reduceFunc)
+        where TValue : notnull
+        where TValueResult : notnull
+    {
+        if (!current.IsSuccess)
+        {
+            return ResultBox.FromException<TValueResult>(current.GetException());
+        }
+        var result = ResultBox.FromValue(initialValue);
+        foreach (var value in current.GetValue())
+        {
+            result = reduceFunc(value, result.GetValue());
+            if (!result.IsSuccess)
+            {
+                break;
+            }
+        }
+        return result.IsSuccess ? ResultBox.FromValue(result.GetValue()) : result.GetException();
+    }
+
+}
