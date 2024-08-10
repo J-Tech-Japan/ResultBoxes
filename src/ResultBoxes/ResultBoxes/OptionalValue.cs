4dc9c4ede25ab5fb2881044a6471b9609fab9d6a
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-08-09 17:57:32 -0700

    Add OptionalValue.FromNullableValue

--- src/ResultBoxes/ResultBoxes/OptionalValue.cs
+++ src/ResultBoxes/ResultBoxes/OptionalValue.cs
@@ -1,14 +1,17 @@
 using System.Text.Json.Serialization;
 namespace ResultBoxes;
 
-public record OptionalValue<TValue>(TValue? Value)
+public record OptionalValue<TValue>(TValue? Value, bool HasValue = true)
 {
-    [JsonIgnore]
-    public bool HasValue => Value is not null;
-
-    public static OptionalValue<TValue> Empty => new(default);
-    public static OptionalValue<TValue> Null => new(default);
-    public TValue GetValue() => Value ?? throw new ResultsInvalidOperationException("no value");
+    public static OptionalValue<TValue> Empty => new(default, false);
+    public static OptionalValue<TValue> Null => new(default, false);
+    public TValue GetValue() => HasValue && Value is not null ? Value : throw new ResultsInvalidOperationException("no value");
     public static implicit operator OptionalValue<TValue>(TValue value) => new(value);
-    public static OptionalValue<TValue> FromValue(TValue value) => new(value);
+}
+public static class OptionalValue
+{
+    public static OptionalValue<TValue> FromValue<TValue>(TValue value) where TValue : notnull => new(value);
+    public static OptionalValue<TValue> FromNullableValue<TValue>(TValue? value) where TValue : notnull => value is null ? OptionalValue<TValue>.Null : new(value);
+    public static OptionalValue<TValue> FromNullableValue<TValue>(TValue? value) where TValue : struct => value.HasValue ? new(value.Value) : OptionalValue<TValue>.Null;
+    public static  ResultBox<TValue> GetValueResult<TValue>(this OptionalValue<TValue> optional) where TValue : notnull => optional.HasValue && optional.Value is not null ? optional.Value : new ResultsInvalidOperationException("no value");
 }
